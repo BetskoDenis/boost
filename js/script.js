8bfb3d88eedc9473ea2e47290dd2af255ee7f2bd
BetskoDenis <47107535+BetskoDenis@users.noreply.github.com>
2019-04-09 20:38:50 +0300

    v0.2

--- js/script.js
+++ js/script.js
@@ -19,22 +19,59 @@ $('.first_slider').owlCarousel({
 
 
 var pauseButton = $('.play_Now'),
+    playIn = $('.video_section_three'),
 // vid = $('.video_section_three'),
     pnow = $('.play_Now');
 
-pauseButton.on('click', function () {
-    $('.video_section_three').css("opacity","1");
+// pauseButton.on('click', function () {
+//     $('.video_section_three').css("opacity","1").toggleClass('play');
+//
+//     if($('.video_section_three').hasClass('play')){
+//         $('.video_section_three').trigger('play');
+//         pnow.html('Paused') ;
+//         $(".play_img").css("opacity","0");
+//     }
+//     else {
+//         pnow.html('Play Now') ;
+//         $('.video_section_three').trigger('pause');
+//         $(".play_img").css("opacity",".5");
+//         $('.video_section_three').css("opacity",".5")
+//     }
+//
+// });
 
-        if($('.video_section_three').trigger('pause')){
+function videoBlock(selector) {
+    selector.on('click', function () {
+        $('.video_section_three').css("opacity","1").toggleClass('play');
+
+        if($('.video_section_three').hasClass('play')){
             $('.video_section_three').trigger('play');
             pnow.html('Paused') ;
             $(".play_img").css("opacity","0");
         }
-        else{
+        else {
             pnow.html('Play Now') ;
             $('.video_section_three').trigger('pause');
-            $(".play_img").css("opacity",".5")
+            $(".play_img").css("opacity",".5");
+            $('.video_section_three').css("opacity",".5")
         }
 
     });
+}
+videoBlock(pauseButton);
+videoBlock(playIn);
 
+$(document).ready(function($) {
+    $('.tab_content').hide();
+    $('.tab_content:first').show();
+    $('.tabs li:first').addClass('active');
+    $('.tabs li').click(function(event) {
+        $('.tabs li').removeClass('active');
+        $(this).addClass('active');
+        $('.tab_content').hide();
+
+        var selectTab = $(this).find('a').attr("href");
+
+        $(selectTab).fadeIn();
+    });
+});
